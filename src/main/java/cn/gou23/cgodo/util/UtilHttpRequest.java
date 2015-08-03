e6854c6e7db44b52ba4684a19131c2b895285c91
liyixing1 <liyixingx@qq.com>
2015-08-03 11:50:52 +0800

    utilrequest

--- src/main/java/cn/gou23/cgodo/util/UtilHttpRequest.java
+++ src/main/java/cn/gou23/cgodo/util/UtilHttpRequest.java
@@ -1,5 +1,8 @@
 package cn.gou23.cgodo.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
@@ -7,13 +10,34 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * 
  * 
- * 描述:客户端请求帮助类
+ * 描述:客户端请求帮助类<br>
+ * 
+ * <ul>
+ * <li>获取客户IP</li>
+ * <li>是否PC</li>
+ * <li>是否微信</li>
+ * <li>是否手机</li>
+ * </ul>
  *
  * @author liyixing
  * @version 1.0
  * @since 2015年8月2日 下午11:24:09
  */
 public class UtilHttpRequest {
+	/**
+	 * 移动设备正则匹配：手机端，用于判断浏览器信息<br>
+	 * \b 是单词边界(连着的两个(字母字符 与 非字母字符) 之间的逻辑上的间隔),<br>
+	 * 字符串在编译时会被转码一次,所以是<br>
+	 * "\\b" \B 是单词内部逻辑间隔(连着的两个字母字符之间的逻辑上的间隔)
+	 */
+	private static final Pattern PAR_PATTERN = Pattern.compile(
+			"\\b(ip(hone|od)|android|opera m(ob|in)i"
+					+ "|windows (phone|ce)|blackberry"
+					+ "|s(ymbian|eries60|amsung)|p(laybook|alm|rofile/midp"
+					+ "|laystation portable)|nokia|fennec|htc[-_]"
+					+ "|mobile|up.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\\b",
+			Pattern.CASE_INSENSITIVE);
+
 	/**
 	 * 
 	 * 描述:获取客户真实的ip
@@ -55,6 +79,57 @@ public class UtilHttpRequest {
 		}
 
 		return ip;
+	}
 
+	/**
+	 * 是否微信
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static final boolean isWechat(HttpServletRequest request) {
+		if (!isMobile(request)) {
+			return false;
+		}
+
+		String ua = getUseragent(request);
+
+		if (StringUtils.isBlank(ua)) {
+			return false;
+		}
+		return ua.toLowerCase().indexOf("micromessenger") > 0;
+	}
+
+	/**
+	 * 是否手机
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static final boolean isMobile(HttpServletRequest request) {
+		String ua = getUseragent(request);
+
+		if (StringUtils.isBlank(ua)) {
+			return false;
+		}
+
+		// 匹配
+		Matcher matcherPhone = PAR_PATTERN.matcher(ua);
+
+		if (matcherPhone.find()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * 获取浏览器的user-agent（浏览器信息）
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static final String getUseragent(HttpServletRequest request) {
+		return request.getHeader("user-agent");
 	}
 }
