792f3ced0173dbd976d68cd90d31dbb8ba2c56c8
liyixing1 <liyixingx@qq.com>
2015-09-01 11:18:34 +0800

    *

--- src/main/java/cn/gou23/cgodo/mybatis/aop/CountAspect.java
+++ src/main/java/cn/gou23/cgodo/mybatis/aop/CountAspect.java
@@ -33,8 +33,7 @@ public class CountAspect {
 	 * @author liyixing 2011-12-6 下午09:47:50
 	 * @throws Throwable
 	 */
-	@Around("execution(* selectByExampleWithRowbounds(.., org.apache.ibatis.session.RowBounds)) || "
-			+ "execution(* selectByExampleWithBLOBsWithRowbounds(.., org.apache.ibatis.session.RowBounds)) ")
+	@Around("execution(public * *(.., org.apache.ibatis.session.RowBounds)) ")
 	public Object count(ProceedingJoinPoint pjp) throws Throwable {
 		ReflectiveMethodInvocation reflectiveMethodInvocation = (ReflectiveMethodInvocation) FieldUtils
 				.getField(pjp.getClass(), "methodInvocation", true).get(pjp);
