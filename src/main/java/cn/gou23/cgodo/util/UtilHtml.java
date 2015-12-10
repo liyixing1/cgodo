cdc7cc16c84c273d63a1b6b145a506b18097df15
liyixing1 <liyixingx@qq.com>
2015-12-10 13:24:44 +0800

    html

--- src/main/java/cn/gou23/cgodo/util/UtilHtml.java
+++ src/main/java/cn/gou23/cgodo/util/UtilHtml.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import org.apache.commons.lang3.StringUtils;
 
