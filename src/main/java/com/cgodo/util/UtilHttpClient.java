887d52ddb7eeea496f7dc29257e7c6cdd7003af4
liyixing1 <liyixingx@qq.com>
2017-05-28 15:07:39 +0800

    wordtoimg

--- src/main/java/com/cgodo/util/UtilHttpClient.java
+++ src/main/java/com/cgodo/util/UtilHttpClient.java
@@ -1,6 +1,8 @@
 package com.cgodo.util;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -8,6 +10,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -60,6 +63,36 @@ public class UtilHttpClient {
 		
 		postEntity.setContentType("text/xml");
         httpPost.addHeader("Content-Type", "text/xml");
+        httpPost.setEntity(postEntity);
+        
+		CloseableHttpResponse response = httpclient.execute(httpPost);
+		HttpEntity entity = response.getEntity();
+		String result= EntityUtils.toString(entity,"utf-8"); 
+		return result;
+	}
+	
+	/**
+	 * post方式请求，发送上传文件数据
+	 * 
+	 * @throws IOException
+	 * @throws ClientProtocolException
+	 */
+	public static final String httpRequestPostFile(String url, Map<String,Object> params) throws Exception {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpPost httpPost = new HttpPost(url);
+		MultipartEntityBuilder postEntityBuilder = MultipartEntityBuilder.create();
+		
+		for(Map.Entry<String, Object> param : params.entrySet()) {
+			//文本
+			if(param.getValue() instanceof String) {
+				postEntityBuilder.addTextBody(param.getKey(), (String) param.getValue());
+			} else {
+				File file = (File) param.getValue();
+				postEntityBuilder.addBinaryBody(param.getKey(), file);
+			}
+		}
+		
+		HttpEntity postEntity = postEntityBuilder.build();
         httpPost.setEntity(postEntity);
         
 		CloseableHttpResponse response = httpclient.execute(httpPost);
---
+++ src/main/java/com/cgodo/util/UtilWord.java
@@ -0,0 +1,55 @@
+package com.cgodo.util;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class UtilWord {
+	/**
+	 * 
+	 * 描述:word转图片
+	 * 
+	 * @param file
+	 * @author liyixing 2017年5月28日 下午1:04:42
+	 * @throws Exception 
+	 */
+	public static final List<String> word2Img(File file) throws Exception {
+		List<String> results = new ArrayList<String>();
+		String url = "http://www.docpe.com/word/word-to-image.aspx";
+		Map<String, Object> params = UtilMisc
+				.toMap("__VIEWSTATE",
+						"/wEPDwULLTE3MTExODk1MTkPZBYCZg9kFgICAw8WAh4HZW5jdHlwZQUTbXVsdGlwYXJ0L2Zvcm0tZGF0YWRkgz013LYu/ivQbeCJHdF5rL7djqk8weXauWmo0Ohccyc=",
+						"__VIEWSTATEGENERATOR",
+						"64DAF92B",
+						"__EVENTVALIDATION",
+						"/wEdAAmhyUgD9FTUMeQJH4N5MdCpCG9mt+G5mKxkJmbtRkfMQTyR1R+fT/IjuebvQlAfObN9iXMNWwR7DLE2yWIT3HqM3XZxIK/NvYMvtFx3AW3s1je5gij1i1GPrY+QGn/6BFUCVVfbMNCH7DygaimgizggFgTjtK7hK11bry/4HktSMDyLlmRoMzte3Qd9tvkDZDpjukRZZop55XYJRyAGuVdBuLlAUrqnbhfR0IqmaBiOBw==",
+						"ctl00$content$FileUpload1", file,
+						"ctl00$content$txtResolution", "400",
+						"ctl00$content$ddlImageType", "png",
+						"ctl00$content$cmdSaveAttachment", "转换");
+		String result = UtilHttpClient.httpRequestPostFile(url, params);
+		Document doc = Jsoup.parse(result);
+		Elements elements = doc.getElementById("ctl00_content_lblNew").getElementsByTag("a");
+		
+		if(elements.size() > 1) {
+			for(int i = 1; i< elements.size(); i++) {
+				Element e = elements.get(i);
+				
+				results.add(e.attr("href"));
+			}
+		}
+		
+		return results;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		word2Img(new File("D:\\1.doc"));
+		word2Img(new File("D:\\2.doc"));
+	}
+}
